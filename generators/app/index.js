55c236ec978e50e7388bccd921c09ee39c6a08f6
Brad Hards <bradh@frogmouth.net>
2018-03-22 21:31:28 +1100

    fix(lint): Code cleanups.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,22 +1,19 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const slugify = require('slugg')
+const slugify = require('slugg');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the ${chalk.red('opensphere plugin')} generator!`)
-    );
+    this.log(yosay(`Welcome to the ${chalk.red('opensphere plugin')} generator!`));
 
     const prompts = [
       {
-        type    : 'input',
-        name    : 'plugin_name',
-        message : 'Your plugin name',
-        default : slugify(this.appname)
+        type: 'input',
+        name: 'pluginName',
+        message: 'Your plugin name',
+        default: slugify(this.appname)
       }
     ];
 
@@ -28,22 +25,38 @@ module.exports = class extends Generator {
 
   default() {
     console.log(this.props);
-    if (this.props.plugin_name.startsWith('opensphere-plugin-')) {
-      this.props.plugin_name = this.props.plugin_name.substring('opensphere-plugin-'.length);
+    if (this.props.pluginName.startsWith('opensphere-plugin-')) {
+      this.props.pluginName = this.props.pluginName.substring(
+        'opensphere-plugin-'.length
+      );
     }
     console.log(this.props);
   }
 
   writing() {
-    var pluginTemplates = ['LICENSE', 'modernizr.config.json', 'package.json', 'config/settings.json', 'views/README.md'];
+    var pluginTemplates = [
+      'LICENSE',
+      'modernizr.config.json',
+      'package.json',
+      'config/settings.json',
+      'views/README.md'
+    ];
     pluginTemplates.forEach(function(templateFile) {
-      this.fs.copyTpl(this.templatePath(templateFile), this.destinationPath(templateFile), this.props);
-    }, this);
-    var srcFiles = ['plugin.js']
-    srcFiles.forEach(function(srcFile) {
-      this.fs.copyTpl(this.templatePath('src/' + srcFile), this.destinationPath('src/plugin/' + this.props.plugin_name + '/' + srcFile), this.props);
+      this.fs.copyTpl(
+        this.templatePath(templateFile),
+        this.destinationPath(templateFile),
+        this.props
+      );
     }, this);
 
+    var srcFiles = ['plugin.js'];
+    srcFiles.forEach(function(srcFile) {
+      this.fs.copyTpl(
+        this.templatePath('src/' + srcFile),
+        this.destinationPath('src/plugin/' + this.props.pluginName + '/' + srcFile),
+        this.props
+      );
+    }, this);
   }
 
   install() {
